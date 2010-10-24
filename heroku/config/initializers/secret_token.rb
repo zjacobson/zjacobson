fd8ba1b76291a4e456ad593e46bf1fe77a58856b
Zak Jacobson <jacobsonz@gmail.com>
2010-10-23 22:47:29 -0700

    moving files into appengine.  adding heroku spike.

---
+++ heroku/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CartoonRails::Application.config.secret_token = '5ea954d05d43d00d79c522a3dc971a31a70ed0ad12f07b0a86c3387da1295f09fe7a424710f874c0923848d470338f92c8a2f8467d69e1073a7b45617016f3a1'
