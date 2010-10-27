14c59c60e5ab3d8bc46f7607400e8f041f00bd00
Zak Jacobson <jacobsonz@gmail.com>
2010-10-27 00:15:36 -0700

    alternative Koala code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
 before_filter :parse_facebook_cookies
 
 def parse_facebook_cookies
-  #@facebook_cookies ||= Koala::Facebook::OAuth.new('161293420568925', '93fee3a1c1d4961402882f3bfb606640').get_user_from_cookie(cookies)
+  @facebook_cookies ||= Koala::Facebook::OAuth.new('161293420568925', '93fee3a1c1d4961402882f3bfb606640').get_user_from_cookie(cookies)
 
   # If you've setup a configuration file as shown above then you can just do
-   @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_from_cookie(cookies)
+  # @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_from_cookie(cookies)
    logger.info "The cookies are #{@facebook_cookies}"
    logger.info "cookies #{@cookies}"
 end
